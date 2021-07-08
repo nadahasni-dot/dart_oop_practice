ffb436bbd0bb0c98f3fd57e0b704748cb568ee54
Nada Hasni Muhammad <nadasthing@gmail.com>
2021-07-08 18:48:21 +0700

    standard io, and greetings

--- bin/dart_oop_practice.dart
+++ bin/dart_oop_practice.dart
@@ -1,4 +1,9 @@
 
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello World');
+  String name = stdin.readLineSync();
+  int age = int.tryParse(stdin.readLineSync());
+
+  print('Hello $name your age is $age years old.');
 }
