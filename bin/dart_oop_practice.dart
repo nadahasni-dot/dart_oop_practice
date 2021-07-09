b5b6544ea4760ecafb427bce749c161ca10b4b13
Nada Hasni Muhammad <nadasthing@gmail.com>
2021-07-09 10:39:02 +0700

    basic class, create instance of class (object), modify object field, and call object method

--- bin/dart_oop_practice.dart
+++ bin/dart_oop_practice.dart
@@ -1,43 +1,33 @@
+//* create class blueprint
+class Rectangle {
+  //* field of class
+  double lenght;
+  double height;
+
+  //* method of class
+  double calculateArea() {
+    //* this keyword is optional on dart
+    return this.lenght * height;
+  }
+}
+
 void main(List<String> arguments) {
-  //* list declaration
-  List<String> myNumbers = [];
-  List<int> list = [1, 3, 4, 7, 2, 5, 10, 8];
+  //* create identifier for object
+  Rectangle rectangle1, rectangle2;
 
-  //* add to list
-  list.add(100);
-  list.addAll([10, 99, 108]);
+  //* create object from class rectangle
+  rectangle1 = Rectangle();
+  //* set rectangle field
+  rectangle1.height = 2;
+  rectangle1.lenght = 3;
+  print('rectangle 1 area: ' + rectangle1.calculateArea().toString());
 
-  //* insert into index
-  list.insert(2, 88);
-  list.insertAll(5, [10, 11, 12]);
+  //* create object from class rectangle
+  rectangle2 = Rectangle();
+  //* set rectangle field
+  rectangle2.height = 10;
+  rectangle2.lenght = 5;
+  print('rectangle 2 area: ' + rectangle2.calculateArea().toString());
 
-  //* remove value from list
-  list.remove(20);
-  list.removeWhere((number) => number % 2 != 0); // remove odd
-  list.removeRange(2, 4); // remove from index 2 & 3. stop before index 4  
-
-  //* iterate each list using for
-  // for (int i = 0; i < list.length; i++) {
-  //   print(list[i]);
-  // }
-  // print('=========');
-
-  //* iterate each list using for in
-  // for (int number in list) {
-  //   print(number);
-  // }
-  // print('=========');
-
-  //* iterate each list using foreach
-  list.forEach((number) {
-    print('number ' + number.toString());
-  });
-  print('=========');
-
-  //* map list into another form
-  myNumbers = list.map((number) => 'my number is ' + number.toString()).toList();
-
-  myNumbers.forEach((myNumber) {
-    print(myNumber);
-  });
+  print('rectangle 1 + rectangle 2: ' + (rectangle1.calculateArea() + rectangle2.calculateArea()).toString());
 }
