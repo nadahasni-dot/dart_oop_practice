a9730125d704e24d371a38ead27e095267a0e186
Nada Hasni Muhammad <nadasthing@gmail.com>
2021-07-10 17:23:15 +0700

    basic static keyword

--- bin/dart_oop_practice.dart
+++ bin/dart_oop_practice.dart
@@ -2,18 +2,26 @@ import 'person.dart';
 
 void main(List<String> arguments) {
   //* create person object and pass name into doHobby
-  var budi = Person('Budi', doHobby: (String name) {
+  var budi = Person('Budi', 200, doHobby: (String name) {
     print('$name is playing game');
   });
   print(budi.name);
+  print(budi.age.toString());
 
   budi.enjoyTime();
 
   //* create person object and ignore passing name into doHobby
-  var joko = Person('Joko', doHobby: (_) {
+  var joko = Person('Joko', 24, doHobby: (_) {
     print('person is playing basketball');
   });
   print(joko.name);
+  print(joko.age.toString());
 
   joko.enjoyTime();
+
+  //*show max age
+  print('Max Age of a Person: ${Person.maxAge.toString()}');
+
+  //! will be error. because maxAge is a member of class not a member of an object
+  //! print('Max Age of joko: ${joko.maxAge}');
 }
--- bin/person.dart
+++ bin/person.dart
@@ -1,9 +1,20 @@
 class Person {
+  //* will be member of object after create instance of this class
   String name;
+  int age;
   Function(String hobby) doHobby;
 
+  //* static means, this field will be member of Class not a member of an Object that created from class
+  static int maxAge = 150;
+
   //* constructor person with optional named parameter
-  Person(this.name, {this.doHobby});
+  Person(this.name, int age, {this.doHobby}) {
+    if (age > maxAge) { // check if age is greater than maxAge
+      this.age = maxAge;
+    } else {
+      this.age = age;
+    }
+  }
 
   void enjoyTime() {
     if (doHobby != null) {
